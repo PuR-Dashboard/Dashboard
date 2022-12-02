1359b166b09cb1401e71d64a4dd5e71117ec75c1
TimM4ster <timmichael.krieg@gmail.com>
2022-12-02 19:22:48 +0100

    Changed path

--- App/pages/map_page.py
+++ App/pages/map_page.py
@@ -4,13 +4,13 @@ from dash import html, dcc
 from dash.dependencies import Input, Output, State
 import numpy as np
 from dash.exceptions import PreventUpdate
-
+import os
 
 html_list = []
 html_list.append(html.H3("Interaktive Karte Test"))
 #----------!!!!!!!!!
 # pfad zur karte manchmal weird muss vorm testen angepasst werden(also wenn was falsch läuft kann gut hier dran liegen)
-html_list.append(html.Iframe(id="karte", srcDoc=open(r"C:\Anni\TU Darmstadt\5. Semester\Bachelorpraktikum\Dashboard\P&R_Karte.html", "r").read(), width="100%", height="600"))
+html_list.append(html.Iframe(id="karte", srcDoc=open(os.path.join(os.path.dirname(__file__), '../../P&R_Karte.html'), "r").read(), width="100%", height="600"))
 
 layout = html.Div(html_list)
 
