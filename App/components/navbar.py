0b267816eadb52039bd0b2c41a969fdd3c48a5d1
AnnestineBrandes <119292382+AnnestineBrandes@users.noreply.github.com>
2023-01-08 13:09:07 +0100

    Add files via upload

---
+++ App/components/navbar.py
@@ -0,0 +1,24 @@
+# Import necessary libraries
+from dash import html
+import dash_bootstrap_components as dbc
+
+
+# Define the navbar structure
+def Navbar():
+
+    layout = html.Div([
+        dbc.NavbarSimple(
+            children=[
+                dbc.NavItem(dbc.NavLink("Map View", href="/map_page", style = {"color":"white"} )),
+                dbc.NavItem(dbc.NavLink("List View", href="/list_page", style = {"color":"white"})),
+            ] ,
+            brand="Park&Ride Dashboard",
+            brand_href="/map_page",
+            color="black",
+            dark=True,
+        ), 
+    ])
+
+    return layout
+
+ 
